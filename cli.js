a40390e25640007485c8c19ed940691499a2942a
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-23 20:16:27 -0400

    Update

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@ args.option('version', 'Minecraft Version', 'latest-release')
   .option('username', 'Minecraft Username', 'Steve')
   .option('uuid', 'Minecraft UUID', '0')
   .option('authToken', 'Minecraft Authentication Token', '0')
-  .option('profile', 'Minecraft Game Directory Name', 'latest-release')
+  .option('profile', 'Minecraft Game Directory Name', 'default')
   .option('demo', 'Enable Demo Mode', false);
 const flags = args.parse(process.argv, {version: false});
 mclauncher(flags);
