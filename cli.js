1c283bb19179e6cd5632377d3e1977fee3ac08e3
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-23 21:21:11 -0400

    Add Authentication

--- cli.js
+++ cli.js
@@ -1,11 +1,29 @@
 const mclauncher = require('./index');
 const args = require('args');
+const request = require('sync-request');
 
 args.option('version', 'Minecraft Version', 'latest-release')
-  .option('username', 'Minecraft Username', 'Steve')
-  .option('uuid', 'Minecraft UUID', '0')
-  .option('authToken', 'Minecraft Authentication Token', '0')
+  .option('email', 'Minecraft Email')
+  .option('password', 'Minecraft Password')
   .option('profile', 'Minecraft Game Directory Name', 'default')
   .option('demo', 'Enable Demo Mode', false);
 const flags = args.parse(process.argv, {version: false});
-mclauncher(flags);
+let res = request('POST', 'https://authserver.mojang.com/authenticate', {
+  json: {
+    agent: {
+      name: 'Minecraft',
+      version: 1
+    },
+    username: flags['email'],
+    password: flags['password']
+  }
+});
+let data = JSON.parse(res.getBody('utf8'));
+mclauncher({
+  version: flags['version'],
+  username: data.selectedProfile.name,
+  uuid: data.selectedProfile.id,
+  authToken: data.accessToken,
+  profile: flags['profile'],
+  demo: flags['demo']
+});
